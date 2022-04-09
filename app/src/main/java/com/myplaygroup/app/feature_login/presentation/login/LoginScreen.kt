09018cdec357871efd86d829bcf2757cc149edf6
VegardAaberge <vegard.aaberge@gmail.com>
2022-04-09 21:16:10 +0800

    Hide the keyboard when the background is tapped

--- app/src/main/java/com/myplaygroup/app/feature_login/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/myplaygroup/app/feature_login/presentation/login/LoginScreen.kt
@@ -1,10 +1,13 @@
 package com.myplaygroup.app.feature_login.presentation.login
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.myplaygroup.app.feature_login.presentation.login.components.LoginButton
@@ -18,11 +21,20 @@ fun LoginScreen(
     navigator: DestinationsNavigator,
     viewModel: LoginViewModel = hiltViewModel()
 ) {
+    val focusManager = LocalFocusManager.current
+
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
+            .clickable(
+                interactionSource = MutableInteractionSource(),
+                indication = null,
+                onClick = {
+                    focusManager.clearFocus()
+                }
+            )
     ) {
         LoginTextFields(
             user = viewModel.user.value,
