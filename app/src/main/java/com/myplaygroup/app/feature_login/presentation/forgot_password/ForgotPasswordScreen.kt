bbd6bfa1ba39003b8ca7e18904a369bff29fc7d0
VegardAaberge <vegard.aaberge@gmail.com>
2022-04-10 19:20:07 +0800

    Create the fields for the forgot password screen

--- app/src/main/java/com/myplaygroup/app/feature_login/presentation/forgot_password/ForgotPasswordScreen.kt
+++ app/src/main/java/com/myplaygroup/app/feature_login/presentation/forgot_password/ForgotPasswordScreen.kt
@@ -1,27 +1,126 @@
 package com.myplaygroup.app.feature_login.presentation.forgot_password
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.myplaygroup.app.R
+import com.myplaygroup.app.feature_login.presentation.forgot_password.components.EmailField
+import com.myplaygroup.app.ui.theme.MyPlaygroupTheme
 import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Destination
 @Composable
-fun ForgotPasswoordScreen(
-    navigator : DestinationsNavigator
-) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
+fun ForgotPasswoordScreen() {
+    val email by remember{
+        mutableStateOf("")
+    }
+
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = {
+                    Text(text = stringResource(id = R.string.send_email))
+                },
+                onClick = {
+
+                },
+                modifier = Modifier,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Send,
+                        contentDescription = "Add note"
+                    )
+                },
+                shape = CircleShape,
+                contentColor = Color.White,
+                backgroundColor = MaterialTheme.colors.primary
+            )
+        }
     ) {
-        Text(
-            text = "Forgot Password",
+        val widthModifier = Modifier
+            .width(400.dp)
+            .padding(horizontal = 30.dp)
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
             modifier = Modifier
-                .align(Alignment.Center)
-        )
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xFFAAFFFF),
+                            Color(0xFFEEFFFF),
+                        )
+                    )
+                ),
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.lock_icon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(200.dp)
+                    .aspectRatio(1f)
+                    .clip(CircleShape)
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                fontSize = 30.sp,
+                text = stringResource(id = R.string.forgot_password_header),
+                modifier = widthModifier,
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = stringResource(id = R.string.forgot_password_explanation),
+                modifier = widthModifier
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            EmailField(
+                textValue = email,
+                onTextChanged = {
+                    
+                },
+                modifier = widthModifier
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyPlaygroupTheme {
+        ForgotPasswoordScreen()
     }
 }
---
+++ app/src/main/java/com/myplaygroup/app/feature_login/presentation/forgot_password/components/EmailField.kt
@@ -0,0 +1,31 @@
+package com.myplaygroup.app.feature_login.presentation.forgot_password.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.myplaygroup.app.R
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Composable
+fun EmailField(
+    textValue: String,
+    onTextChanged: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    OutlinedTextField(
+        value = textValue,
+        placeholder = { Text(text = stringResource(id = R.string.forgot_password_placeholder)) },
+        label = { Text(text = stringResource(id = R.string.forgot_password_label)) },
+        singleLine = true,
+        onValueChange = { onTextChanged },
+        modifier = modifier
+    )
+}
