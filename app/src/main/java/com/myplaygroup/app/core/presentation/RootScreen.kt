c5a674817c0a05d433642959a2fcb637e836b50d
VegardAaberge <vegard.aaberge@gmail.com>
2022-05-17 12:03:21 +0800

    Make sure it only logs into MainScreen if access token, refresh token and profile name can be found

--- app/src/main/java/com/myplaygroup/app/core/presentation/RootScreen.kt
+++ app/src/main/java/com/myplaygroup/app/core/presentation/RootScreen.kt
@@ -1,8 +1,10 @@
 package com.myplaygroup.app.core.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.myplaygroup.app.destinations.*
+import com.myplaygroup.app.feature_login.presentation.create_profile.CreateProfileScreen
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
@@ -13,10 +15,12 @@ fun RootScreen(
     navigator: DestinationsNavigator,
     viewModel: RootViewModel = hiltViewModel(),
 ) {
-    if(viewModel.isAuthenticated()){
-        popAndNavigateTo(navigator, MainScreenDestination)
-    }else{
-        popAndNavigateTo(navigator, LoginScreenDestination)
+    LaunchedEffect(key1 = true){
+        if(viewModel.isAuthenticated()){
+            popAndNavigateTo(navigator, MainScreenDestination)
+        }else{
+            popAndNavigateTo(navigator, LoginScreenDestination)
+        }
     }
 }
 
--- app/src/main/java/com/myplaygroup/app/core/presentation/RootViewModel.kt
+++ app/src/main/java/com/myplaygroup/app/core/presentation/RootViewModel.kt
@@ -1,8 +1,12 @@
 package com.myplaygroup.app.core.presentation
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import com.myplaygroup.app.core.data.remote.BasicAuthInterceptor
+import com.myplaygroup.app.core.util.Constants
 import com.myplaygroup.app.core.util.Constants.KEY_ACCESS_TOKEN
+import com.myplaygroup.app.core.util.Constants.KEY_PROFILE_NAME
+import com.myplaygroup.app.core.util.Constants.KEY_REFRESH_TOKEN
 import com.myplaygroup.app.core.util.Constants.NO_VALUE
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -15,12 +19,19 @@ class RootViewModel @Inject constructor(
 
     fun isAuthenticated(): Boolean {
         val currentAccessToken = sharedPref.getString(KEY_ACCESS_TOKEN, NO_VALUE) ?: NO_VALUE
+        val currentRefreshToken = sharedPref.getString(KEY_REFRESH_TOKEN, NO_VALUE) ?: NO_VALUE
+        val profileName = sharedPref.getString(KEY_PROFILE_NAME, NO_VALUE) ?: NO_VALUE
 
-        val isAuthenticated = currentAccessToken != NO_VALUE
-        if(isAuthenticated){
-            basicAuthInterceptor.accessToken = currentAccessToken
+        if(currentAccessToken == NO_VALUE || currentRefreshToken == NO_VALUE || profileName == NO_VALUE){
+            sharedPref.edit {
+                putString(KEY_ACCESS_TOKEN, NO_VALUE)
+                putString(KEY_REFRESH_TOKEN, NO_VALUE)
+                apply()
+            }
+            return false
         }
 
-        return isAuthenticated
+        basicAuthInterceptor.accessToken = currentAccessToken
+        return true
     }
 }
